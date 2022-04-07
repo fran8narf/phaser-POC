174c0f99e0cf22e8ddf144e7508c95bbb302ade6
EneDNarf <srlk_doblev@hotmail.com>
2022-04-07 17:23:06 +0200

    mouse and spacebar input events added

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -9,7 +9,7 @@ const config = {
         // Arcade physics plugin
         default: 'arcade',
         arcade: {
-        gravity: { y : 200},
+        gravity: { y : 600},
         debug: true
         }
     },
@@ -39,16 +39,16 @@ function create() {
 
     console.log(bird.body);
     // input events for mouse
-    this.input.on('pointerdown', () => {
-        console.log('pressing mouse button');
-    })
+    this.input.on('pointerdown', flap)
 
-    this.input.keyboard.on('keydown_SPACE', () => {
-        console.log('pressing space bar');
-    });
+    this.input.keyboard.on('keydown_SPACE', flap);
 }
 
 // 60fps
 // 60 times per second 
 function update(time, delta) {
+}
+
+function flap () {
+    bird.body.velocity.y = -VELOCITY;
 }
