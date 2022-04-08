8e42c3f9a4b9b249c39a965b7679a5538d12198a
EneDNarf <srlk_doblev@hotmail.com>
2022-04-08 12:04:13 +0200

    const to be used as shared config between scenes + created new PlayScene sharing config info through the constructor

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -1,17 +1,29 @@
 import Phaser from 'phaser';
 import PlayScene from './scenes/PlayScene';
 
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = {
+    x: WIDTH / 10,
+    y: HEIGHT / 2
+};
+
+const SHARED_CONFIG = {
+    width: WIDTH,
+    height: HEIGHT,
+    startPosition : BIRD_POSITION
+};
+
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    ...SHARED_CONFIG,
     physics: {
         default: 'arcade',
         arcade: {
             debug: true
         }
     },
-    scene: [PlayScene]
+    scene: [new PlayScene(SHARED_CONFIG)]
 }
 new Phaser.Game(config);
 
